f6cec3ec517aa39ea170253fff8327a273a47cd3
Mrigank Gupta <mrigank.gupta@progfin.in>
2025-08-11 22:37:43 +0530

    notes_aug_11

---
+++ python_learning/aug_11.py
@@ -0,0 +1,80 @@
+# Data Types in Python
+
+# print("String")
+
+# name = "Tony Stark"
+# print(name)
+# age = "27"
+# print(age," The type is " ,type(age))
+# gender = 'Female'
+# print(gender)
+# sentence = "I haven't told my parents about your's"
+# print(sentence)
+# paragraph = """
+#     Hello
+#     I
+#     am 
+#     THOR
+# """
+# print(paragraph)
+
+# print("Numeric DataType")
+# age = 27
+# print(age, type(age))
+# marks = 69.5
+# print(marks, type(marks))
+# complex_variable = 6+5j
+# print(complex_variable, type(complex_variable))
+
+# percentage = 485/100
+# print(percentage)
+
+# a = True
+# print(a, type(a))
+# b = False
+# print(b, type(b))
+
+# print("Type Casting")
+
+num = 10
+perc = 69.5
+flag = True
+name = "Thor"
+
+# Type casting into integer
+# print(int(num))
+# print(float(num))
+# print(bool(num))
+# print(str(num))
+
+# Type casting into float
+# print(int(perc))
+# print(float(perc))
+# print(bool(perc))
+# print(str(perc))
+
+# Type casting into bool
+# print(int(flag))
+# print(float(flag))
+# print(bool(flag))
+# print(str(flag))
+
+# Type casting into string
+# print(int(name))
+# print(float(name))
+# print(bool(name))
+# print(str(name))
+
+# print("Sequence Type")
+
+# a = []
+# print(a, type(a))
+# b = list()
+# print(b, type(b))
+
+# list1 = [1, 2, 3, 4, 5]
+# print(list1)
+
+# list2 = list(6, 7, 8, 9)
+# print(list2)
+
